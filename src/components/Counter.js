b96498875f22d01c7882ce0f413fcb3c188556c1
vguenoff <vgenoff@gmail.com>
2017-05-06 15:03:49 +0300

    the simplest redux example finished

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+// import { Component } from 'react';
 
-class Counter extends Component {
-  static propTypes = {
-    value: PropTypes.number.isRequired,
-    onIncrement: PropTypes.func.isRequired,
-    onDecrement: PropTypes.func.isRequired
-  };
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
 
-  render() {
-    const { value, onIncrement, onDecrement } = this.props;
-    return (
-      <div>
-        <p>Value: </p>
-        <h2>
-          {value}
-        </h2>
-        <button onClick={onIncrement}>
-          +
-        </button>
-        <button onClick={onDecrement}>
-          -
-        </button>
-      </div>
-    );
-  }
-}
+// import PropTypes from 'prop-types';
+//
+// class Counter extends Component {
+//   static propTypes = {
+//     value: PropTypes.number.isRequired,
+//     onIncrement: PropTypes.func.isRequired,
+//     onDecrement: PropTypes.func.isRequired
+//   };
+//
+//   render() {
+//     const { value, onIncrement, onDecrement } = this.props;
+//     return (
+//       <div>
+//         <p>Value: </p>
+//         <h2>
+//           {value}
+//         </h2>
+//         <button onClick={onIncrement}>
+//           +
+//         </button>
+//         <button onClick={onDecrement}>
+//           -
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default Counter;
